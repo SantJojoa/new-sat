45bcb3612a5c03204d4fde656bbcca428752ec35
Santiago Jojoa <santiago.jojoan@gmail.com>
2026-09-02 09:51:42 -0500

    fix:fixed creating email function

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -163,7 +163,7 @@ export class UsersService {
     }
 
     async findByEmail(email: string) {
-        return await this.prisma.users.findUnique({
+        return await this.prisma.users.findFirst({
             where: {
                 email
             },
